6d7e0e512c39930dcadb0f4d8ad5cce2e8b48276
jaden.callewaert <jaden.callewaert@student.howest.be>
2021-02-07 14:00:24 +0100

    setup the NavigationContainer + the stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from "./screens/Home";
+import Form from "./screens/Form";
+import Details from "./screens/Details";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Home">
+            {props => <Home {...props}/>}
+          </Stack.Screen>
+          <Stack.Screen name="Form">
+            {props => <Form {...props}/>}
+          </Stack.Screen>
+          <Stack.Screen name="Details">
+            {props => <Details {...props}/>}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
