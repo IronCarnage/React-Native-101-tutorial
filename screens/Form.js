a75a2cc8e4021f40b12e3c781f40f288f4e9d343
jaden.callewaert <jaden.callewaert@student.howest.be>
2021-02-07 14:42:09 +0100

    implemented form.js

--- screens/Form.js
+++ screens/Form.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View , TouchableOpacity} from 'react-native';
+import React, {useEffect} from 'react';
+import { StyleSheet, Text, View , TouchableOpacity, TextInput} from 'react-native';
+import { useForm } from 'react-hook-form'
 
 const Form = (props) => {
+    const {register, handleSubmit, setValue} = useForm();
+
+    useEffect(() =>{
+        register('name');
+    }, [register]);
+
+    const showDetail = (data) => {
+      //TODO navigate to details with correct data
+        console.log(data);
+    };
 
     return (
         <View style={styles.container}>
-
+            <Text style={styles.text}>Name:</Text>
+            <TextInput onChangeText={text => {
+                setValue('name', text)
+            }}/>
+            <TouchableOpacity onPress={handleSubmit(showDetail)}>
+                <Text>Show details</Text>
+            </TouchableOpacity>
         </View>
     );
-
 };
 
 export default Form;
